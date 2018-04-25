d502e805dcfe55a6c00fb896c648ba84e4b7a945
Frederic Cambon <cambonf@gmail.com>
2018-04-25 08:29:06 +0200

    Adds prettier js, updates packages

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,37 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import ROMList from './ROMList';
-import SearchROM from './SearchROM';
-
-
+import ROMList from "./ROMList";
+import SearchROM from "./SearchROM";
 
 class Menu extends React.Component {
   onSearchFilter = roms => {
-        this.props.onSearchFilter( roms );
-    }
+    this.props.onSearchFilter(roms);
+  };
 
-    onSearchSelect = rom => {
-        this.props.onSearchSelect( rom );
-    }
+  onSearchSelect = rom => {
+    this.props.onSearchSelect(rom);
+  };
 
-    onSearchClose = () => {
-        this.props.onSearchClose();
-    }
+  onSearchClose = () => {
+    this.props.onSearchClose();
+  };
 
-    render() {
-        var navBarStyle = {
-            borderRadius: 0
-        };
+  render() {
+    var navBarStyle = {
+      borderRadius: 0
+    };
 
-        return (
-          <nav class="navbar navbar-dark bg-dark">
-            <div class="container">
-              <a class="navbar-brand" href="#">NES</a>
-            </div>
-          </nav>
-        )
-    }
-
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            NES
+          </a>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Menu
+export default Menu;
--- src/components/NES.js
+++ src/components/NES.js
@@ -1,83 +1,87 @@
-import _ from 'lodash';
-import Console from 'nes';
+import _ from "lodash";
+import Console from "nes";
 
-import React from 'react';
-import {
-    withRouter
-} from 'react-router'
+import React from "react";
+import { withRouter } from "react-router";
 
-import Screen from './Screen';
-import Menu from './Menu';
-import HelpModal from './HelpModal';
-import {
-    ROMS
-} from '../utils/constants';
-
+import Screen from "./Screen";
+import ThreeJS3DScreen from "./ThreeJS3DScreen";
+import Menu from "./Menu";
+import HelpModal from "./HelpModal";
+import { ROMS } from "../utils/constants";
 
 class NES extends React.Component {
-    /*
-     * Main component for the NES, handles the console status
-     */
+  /*
+   * Main component for the NES, handles the console status
+   */
 
-    constructor( props ) {
-        super( props );
+  constructor(props) {
+    super(props);
 
-        this.rom = ROMS[ _.findIndex( ROMS, [ 'slug', this.props.match.params.game ] ) ];
-        this.console = new Console();
-        //this.props.router.setRouteLeaveHook( this.props.route, () => {
-        //  return 'You have an ongoing game, are you sure you want to leave this page?';
-        //} );
-        this.state = {
-            showHelpModal: false
-        };
-    }
+    this.rom = ROMS[_.findIndex(ROMS, ["slug", this.props.match.params.game])];
+    this.console = new Console();
+    //this.props.router.setRouteLeaveHook( this.props.route, () => {
+    //  return 'You have an ongoing game, are you sure you want to leave this page?';
+    //} );
+    this.state = {
+      showHelpModal: false
+    };
+  }
 
-    boot() {
-        this.console.loadROM( this.rom.filepath ).then( function ( res ) {
-            this.console.start()
-        }.bind( this ) );
-    }
+  boot() {
+    this.console.loadROM(this.rom.filepath).then(
+      function(res) {
+        this.console.start();
+      }.bind(this)
+    );
+  }
 
-    componentDidMount() {
-        this.boot()
-    }
+  componentDidMount() {
+    this.boot();
+  }
 
-    componentWillUnmount() {
-        this.console.stop()
-    }
+  componentWillUnmount() {
+    this.console.stop();
+  }
 
-    render() {
-        var cardBackgroundStyle = {
-            width: "100%",
-            height: "100%",
-            backgroundImage: 'url(' + this.rom.cover + ')',
-            backgroundSize: "cover"
-        }
+  render() {
+    var cardBackgroundStyle = {
+      //  width: "100%",
+      //    height: "100%",
+      //  backgroundImage: 'url(' + this.rom.cover + ')',
+      //      backgroundSize: "cover"
+    };
 
-        return (
-            <div>
-                <HelpModal show={this.state.showHelpModal} onClose={() => {this.setState({showHelpModal: false})}} />
-                <div className="jumbotron" style={{'margin': '1rem'}}>
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <h1 className="page-header">{this.rom.label}</h1>
-                            </div>
-                        </div>
-                        <div className="row" style={{paddingTop: "20px"}}>
-                            <div className="col-xl-8" style={{padding: 0}}>
-                                <Screen console={this.console} onHelpClick={() => {this.setState({showHelpModal: true})}} />
-                            </div>
-                            <div className="col-xl-4">
-                                <div style={cardBackgroundStyle}></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    return (
+      <div>
+        <HelpModal
+          show={this.state.showHelpModal}
+          onClose={() => {
+            this.setState({ showHelpModal: false });
+          }}
+        />
+        <div className="jumbotron" style={{ margin: "1rem" }}>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-lg-12">
+                <h1 className="page-header">{this.rom.label}</h1>
+              </div>
             </div>
-        )
-    }
-
+            <div className="row" style={{ paddingTop: "20px" }}>
+              <div className="col-xl-8" style={{ padding: 0 }}>
+                <ThreeJS3DScreen
+                  console={this.console}
+                  onHelpClick={() => {
+                    this.setState({ showHelpModal: true });
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default withRouter( NES )
+export default withRouter(NES);
