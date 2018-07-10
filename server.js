b1e5897d9a7b4db0719d5f8b741f5aef83bf7875
Frederic Cambon <cambonf@gmail.com>
2018-07-10 09:48:57 +0200

    Adds const port in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "build")));
 app.use("/data", express.static(path.join(__dirname, "data")));
@@ -9,4 +10,4 @@ app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(8080, () => console.log("Listening on port 8080"));
+app.listen(port, () => console.log("Listening on port " + port));
