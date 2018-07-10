c6e9817ebddfc9d11f03a8b854f039c21818e569
Frederic Cambon <cambonf@gmail.com>
2018-07-10 09:33:26 +0200

    Updates server.js port

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(8080, () => console.log("Listening on port 8080"));
