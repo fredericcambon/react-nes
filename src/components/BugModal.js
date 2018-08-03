ee3fdee8573a25808426780fe3f52cbd83d27461
Frederic Cambon <cambonf@gmail.com>
2018-08-03 18:24:29 +0200

    Updates bug modal message

--- src/components/BugModal.js
+++ src/components/BugModal.js
@@ -4,9 +4,10 @@ import Modal from "./Modal";
 
 class BugModal extends React.Component {
   render() {
+    var link = "https://github.com/fredericcambon/nes";
     return (
       <Modal show={this.props.show} onClose={this.props.onClose} title="Found a bug?">
-        TODO
+        Please open an issue here if you find any bug with the emulator: <a href={link}>{link}</a>
       </Modal>
     );
   }
